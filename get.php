23cd1642f79266421b0550e551752a13e6d3552d
geraphl <ralle_666@web.de>
2016-08-25 14:14:44 +0200

    protected from sql injection, added some send commands

--- get.php
+++ get.php
@@ -17,11 +17,12 @@
 	if (isset($_REQUEST["global"])) {
 		$where_stat = "";
 		if ($_REQUEST["global"] !== "") {
-			$where_stat = "WHERE VARIABLE='" . $_REQUEST["global"] . "'";
-		}		
-		$sql = "SELECT * FROM `Global_Settings` " . $where_stat;
-		$data = $db->query($sql);
-		$array = $data->fetchAll(PDO::FETCH_COLUMN|PDO::FETCH_GROUP);
+			$where_stat = "WHERE VARIABLE=:variab";
+		}
+		$stmt = $db->prepare("SELECT * FROM `Global_Settings` $where_stat");
+		$stmt->bindValue(':variab', $_REQUEST["global"]);
+		$stmt->execute();	
+		$array = $stmt->fetchAll(PDO::FETCH_COLUMN|PDO::FETCH_GROUP);
 		echo json_encode($array);
 		return;
 	}
@@ -29,25 +30,26 @@
 	if (isset($_REQUEST["device"])) {
 		$where_stat = "";
 		if ($_REQUEST["device"] !== "") {
-			$where_stat = "WHERE dn.DEVICE_ID='" . $_REQUEST["device"] . "'";
-		}	
-		$sql = "SELECT dn.DEVICE_ID, DEVICE_NAME, ds.SWITCHER_ID, DIMMABLE, DEVICE_LAST_ON, DEVICE_LAST_OF, IF( DEVICE_LAST_ON < DEVICE_LAST_OF OR 
-				DEVICE_LAST_ON < DATE_SUB(NOW(), INTERVAL $TIME_DIF_FOR_OFFLINE MINUTE) ,'OFF','ON') AS STATUS 
+			$where_stat = "WHERE dn.DEVICE_ID=:device";
+		}		
+		$stmt = $db->prepare("SELECT dn.DEVICE_ID, DEVICE_NAME, ds.SWITCHER_ID, DIMMABLE, POWER_STATUS, DEVICE_LAST_ON, DEVICE_LAST_OF, IF( DEVICE_LAST_ON < DEVICE_LAST_OF OR 
+				DEVICE_LAST_ON < DATE_SUB(NOW(), INTERVAL :TIME_DIF_FOR_OFFLINE MINUTE) ,'OFF','ON') AS STATUS 
 				FROM `Device_Name` AS dn 
 				LEFT JOIN `Device_Switch` AS ds ON ds.DEVICE_ID=dn.DEVICE_ID
-				LEFT JOIN `Switch_Dimmable` AS sd ON ds.SWITCHER_ID=sd.SWITCHER_ID
-				$where_stat";
-		$data = $db->query($sql);
-		$array = $data->fetchAll(PDO::FETCH_ASSOC);			
+				$where_stat");
+		$stmt->bindValue(':device', $_REQUEST["device"]);
+		$stmt->bindValue(':TIME_DIF_FOR_OFFLINE', $TIME_DIF_FOR_OFFLINE);
+		$stmt->execute();
+		$array = $stmt->fetchAll(PDO::FETCH_ASSOC);			
 		echo json_encode($array);
 		return;
 	}
 	
 	if (isset($_REQUEST["vals"]) && $_REQUEST["vals"] !== "") {
-		$d_id = $_REQUEST["vals"];		
-		$sql = "SELECT * FROM `Device_Value` WHERE DEVICE_ID='$d_id'";
-		$data = $db->query($sql);
-		$tmp_val = $data->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_ASSOC);
+		$stmt = $db->prepare("SELECT * FROM `Device_Value` WHERE DEVICE_ID=:d_id");
+		$stmt->bindValue(':d_id', $_REQUEST["vals"]);
+		$stmt->execute();		
+		$tmp_val = $stmt->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_ASSOC);
 		echo json_encode($tmp_val);
 		return;
 	}
--- send.php
+++ send.php
@@ -13,63 +13,134 @@
 	ini_set('display_errors', 'On');
 	include "constants.php";
 
-
+	//TODO: REMOVE	
+	$commFile = "/home/python/server/commandTransfer";
+	
 	if (isset($_REQUEST["global"]) && $_REQUEST["global"] !== "" // global variable
 		&& isset($_REQUEST["value"]) && $_REQUEST["value"] !== "") {
-			$value=$_REQUEST["value"];
-			$global=$_REQUEST["global"];
-			$sql = "UPDATE Global_Settings
-				SET V_VALUE='$value'
-				WHERE VARIABLE='$global' ";
-			$db->query($sql);
+		$stmt = $db->prepare("UPDATE Global_Settings
+			SET V_VALUE=:value
+			WHERE VARIABLE=:global");
+		$stmt->bindValue(':value', $_REQUEST["value"]);
+		$stmt->bindValue(':global', $_REQUEST["global"]);
+		$stmt->execute();
 		return;
 	}
 	
 	if (isset($_REQUEST["deviceid"]) && $_REQUEST["deviceid"] !== ""  // rename a davice	
 		&& isset($_REQUEST["name"]) && $_REQUEST["name"] !== "") {
-		$name=$_REQUEST["name"];
-		$deviceid=$_REQUEST["deviceid"];
-		$sql = "UPDATE Device_Name
-			SET DEVICE_NAME='$name'
-			WHERE DEVICE_ID=$deviceid";
-		$db->query($sql);	
+		$stmt = $db->prepare("UPDATE Device_Name
+			SET DEVICE_NAME=:name
+			WHERE DEVICE_ID=:deviceid");
+		$stmt->bindValue(':name', $_REQUEST["name"]);
+		$stmt->bindValue(':deviceid', $_REQUEST["deviceid"]);
+		$stmt->execute();
 		return;
 	}
 
 	if (isset($_REQUEST["deviceid"]) && $_REQUEST["deviceid"] !== ""  // rename a davice variable
 		&& isset($_REQUEST["valid"]) && $_REQUEST["valid"] !== ""
 		&& isset($_REQUEST["name"]) && $_REQUEST["name"] !== "") {
-		$name=$_REQUEST["name"];
-		$deviceid=$_REQUEST["deviceid"];
-		$valid=$_REQUEST["valid"];		
-		$sql = "UPDATE Device_Value
-			SET VALUE_NAME='$name'
-			WHERE DEVICE_ID=$deviceid AND VALUE_ID=$valid";
-		$db->query($sql);
+		$stmt = $db->prepare("UPDATE Device_Value
+			SET VALUE_NAME=:name
+			WHERE DEVICE_ID=:deviceid AND VALUE_ID=:valid");
+		$stmt->bindValue(':name', $_REQUEST["name"]);
+		$stmt->bindValue(':deviceid', $_REQUEST["deviceid"]);
+		$stmt->bindValue(':valid', $_REQUEST["valid"]);
+		$stmt->execute();
 		return;
 	}
 	
 	if (isset($_REQUEST["deviceid"]) && $_REQUEST["deviceid"] !== ""  // delete device
-		&& isset($_REQUEST["delete"]) {
-		$deviceid=$_REQUEST["deviceid"]; 
-	 
-	 //delete device
-		$sql = "DELETE FROM `Device_Name` WHERE DEVICE_ID = '$deviceid'";
-		$db->query($sql);
-		$sql = "DELETE FROM `Device_Value` WHERE DEVICE_ID = '$deviceid'";
-		$db->query($sql);
+		&& isset($_REQUEST["delete"])) {
+		$stmt = $db->prepare("DELETE FROM `Device_Name` WHERE DEVICE_ID=:deviceid");
+		$stmt->bindValue(':deviceid', $_REQUEST["deviceid"]);
+		$stmt->execute();
+		$stmt = $db->prepare("DELETE FROM `Device_Value` WHERE DEVICE_ID=:deviceid");
+		$stmt->bindValue(':deviceid', $_REQUEST["deviceid"]);
+		$stmt->execute();		
 		return;
-	 }
+	}
 	 
+	if (isset($_REQUEST["deviceid"]) && $_REQUEST["deviceid"] !== ""  // power
+		&& isset($_REQUEST["power"]) && $_REQUEST["power"] !== "") {
+		$deviceid=$_REQUEST["deviceid"];
+		$power=$_REQUEST["power"];
+		if ($power < 0 || $power > 100) return;
+		$stmt = $db->prepare("UPDATE  Device_Switch
+			SET POWER_STATUS=:power
+			WHERE DEVICE_ID=:deviceid");
+		$stmt->bindValue(':deviceid', $deviceid);
+		$stmt->bindValue(':power', $power);
+		$stmt->execute();	
+		
+		//TODO: REMOVE
+		
+		$sql = "SELECT DIMMABLE FROM `Device_Switch` WHERE DEVICE_ID=$deviceid";
+		$data = $db->query($sql);
+		$array = $data->fetchAll(PDO::FETCH_COLUMN);
+		$dimmable=$array[0];
+		
+		$value="0";
+		if ($dimmable == 1) {
+			if ($power == 0) {
+				$value="OFFDIM";
+			} else if ($power == 100) {
+				$value="ONDIM";
+			} else {
+				$value=$power;
+			}
+		} else {
+			if ($power == 0) {
+				$value="OFF";
+			} else if ($power == 100) {
+				$value="ON";
+			} else {
+				$value=$power;
+			}
+		}
+		file_put_contents ($commFile, "P" . $deviceid . "\x05" . $value );		
+		return;
+	}
 	
+	if (isset($_REQUEST["deviceid"]) && $_REQUEST["deviceid"] !== ""  // set dimmable
+		&& isset($_REQUEST["dimmable"]) && $_REQUEST["dimmable"] !== "") {
+		$dimmable=$_REQUEST["dimmable"];
+		if ($dimmable != 0 && $dimmable != 1) return;		
+		$stmt = $db->prepare("UPDATE  Device_Switch
+			SET DIMMABLE=:dimmable
+			WHERE DEVICE_ID=:deviceid");
+		$stmt->bindValue(':deviceid', $_REQUEST["deviceid"]);
+		$stmt->bindValue(':dimmable', $dimmable);
+		$stmt->execute();	
+		return;
+	}
 	
+	if (isset($_REQUEST["deviceid"]) && $_REQUEST["deviceid"] !== ""  // set switcher
+		&& isset($_REQUEST["switcher"]) && $_REQUEST["switcher"] !== "") {
+		$switcher=$_REQUEST["switcher"];
+		
+		$stmt = $db->prepare("SELECT `V_VALUE` FROM `Global_Settings` WHERE `VARIABLE`='MAX_SWITCHES'");
+		$stmt->execute();	
+		$array = $stmt->fetchAll(PDO::FETCH_COLUMN);
+		$max_switches=$array[0];
+		
+		if ($switcher > $max_switches) return;
+		$stmt = $db->prepare("UPDATE  Device_Switch
+			SET SWITCHER_ID=:switcher
+			WHERE DEVICE_ID=:deviceid");
+		$stmt->bindValue(':deviceid', $_REQUEST["deviceid"]);
+		$stmt->bindValue(':switcher', $switcher);
+		$stmt->execute();		
+		return;
+	}
 	
 	
 	
 	//---------------TO DELETE ------------------
 	
 	
-	
+	/*
 	
 	$commFile = "/home/python/server/commandTransfer";
 	$show_output = true;
@@ -120,7 +191,7 @@
 		
 		if ($name == "POWER") {
 			/*$on = false;
-			if ($value == "ON") $on = true; */			
+			if ($value == "ON") $on = true; */		/*	
 			file_put_contents ($commFile, "P" . $device . "\x05" . $value );
 			
 			return;
@@ -155,7 +226,7 @@
 				exec($t_command, $out, $ret);
 				$out = implode("<br>",$out);
 				if ($show_output) echo "Output: <br> $out <br> Return: $ret";
-				echo ("command: $t_command <br>"); */
+				echo ("command: $t_command <br>"); */ /*
 			}
 		} else {
 			$sql = "INSERT INTO Device_Value
@@ -189,5 +260,5 @@
 				WHERE VARIABLE='$global' ";
 			$db->query($sql);	
 		}
-	}
+	} */
 ?>
